222207326a590312819e0e97cfd127c1234d5e1b
stfnmangini <57354846+stfnmangini@users.noreply.github.com>
2020-06-15 19:35:48 +0200

    Minor fix

--- main.py
+++ main.py
@@ -35,10 +35,12 @@ results = np.array(results)
 mean_blobs = np.mean(results, axis=0)
 std_blobs = np.sqrt(np.var(results, axis = 0))
 
-savetxt('Results.csv', results)
+savetxt('Results.csv', results, fmt='%10.5f')
 
 print("Mean Blobs per day:", mean_blobs)
 print("Var Blobs per day:", std_blobs)
 
-plt.errorbar(np.arange(30), mean_blobs, yerr=std_blobs, color='green', marker='o', linestyle='dashed', linewidth=2, markersize=4, ecolor='red')
+plt.hlines(70, xmin=0, xmax=30, linestyle='dashed', linewidth=1, label='Food Level')
+plt.errorbar(np.arange(30), mean_blobs, yerr=std_blobs, color='green', marker='o', linestyle='dashed', linewidth=2, markersize=4, ecolor='red', label='Blobs')
+plt.legend()
 plt.show()
